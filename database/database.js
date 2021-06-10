825e4f5f1cd1dd23c569c8314881af75b00d4f2e
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-10 15:38:21 -0300

    time zone update

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize")
 
 const connection = new Sequelize('blog', 'root', 'red12mov', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 })
 
 module.exports = connection
