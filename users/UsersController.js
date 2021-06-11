13c49e481c4147b3fc7d3e1bd68f1ca0dcc36cb3
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-11 16:22:16 -0300

    hash de senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,4 +1,5 @@
 const routes = require("express").Router()
+const bcrypt = require("bcryptjs")
 const User = require("./User")
 
 routes.get("/admin/users", (req, res) => {
@@ -12,7 +13,18 @@ routes.get("/admin/users/create", (req, res) => {
 routes.post("/users/create", (req, res) => {
     let email = req.body.email
     let password = req.body.password
-    res.json({email, password})
+
+    let salt = bcrypt.genSaltSync(10)
+    let hash = bcrypt.hashSync(password, salt)
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/")
+    }).catch(err => {
+        res.redirect("/")
+    })
 })
 
 module.exports = routes
